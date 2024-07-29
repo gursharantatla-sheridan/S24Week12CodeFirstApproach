7c507fd397a28b3cd70ae294969c56d4458a4584
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-29 17:19:00 -0400

    complete

--- S24Week12CodeFirstApproach/MainWindow.xaml.cs
+++ S24Week12CodeFirstApproach/MainWindow.xaml.cs
@@ -16,9 +16,45 @@ namespace S24Week12CodeFirstApproach
     /// </summary>
     public partial class MainWindow : Window
     {
+        // define object of context class
+        StudentContext db = new StudentContext();
+
         public MainWindow()
         {
             InitializeComponent();
+            PopulateStandardComboBox();
+        }
+
+        private void PopulateStandardComboBox()
+        {
+            cmbStandard.ItemsSource = db.Standards.ToList();
+            cmbStandard.DisplayMemberPath = "Name";
+            cmbStandard.SelectedValuePath = "StandardId";
+        }
+
+        private void btnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            grdStudents.ItemsSource = db.Students.ToList();
+
+            // hide unwanted columns
+            grdStudents.Columns[4].Visibility = Visibility.Hidden;
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            Student std = new Student();
+            std.Name = txtName.Text;
+            std.StandardId = (int)cmbStandard.SelectedValue;
+            std.Age = 8;
+
+            db.Students.Add(std);
+            db.SaveChanges();
+
+            // refresh data grid
+            grdStudents.ItemsSource = db.Students.ToList();
+            grdStudents.Columns[4].Visibility = Visibility.Hidden;
+
+            MessageBox.Show("New student added");
         }
     }
 }
