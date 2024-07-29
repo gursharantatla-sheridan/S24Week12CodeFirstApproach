8893d31a1fb064a3dcff2acd949e2e94f141934e
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-29 17:03:28 -0400

    design ui

---
+++ S24Week12CodeFirstApproach/Migrations/20240729210012_ChangeColumnStudentName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace S24Week12CodeFirstApproach.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnStudentName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Students",
+                newName: "StudentName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StudentName",
+                table: "Students",
+                newName: "Name");
+        }
+    }
+}
