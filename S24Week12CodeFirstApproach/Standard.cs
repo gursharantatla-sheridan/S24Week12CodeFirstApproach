e40d93bda1b5ce73cf4412b863485885113e7ef8
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-29 16:05:16 -0400

    Init

---
+++ S24Week12CodeFirstApproach/Standard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S24Week12CodeFirstApproach
+{
+    public class Standard
+    {
+        // scalar properties
+        public int StandardId { get; set; }
+        public string? Name { get; set; }
+
+        // navigation property
+        public ICollection<Student>? Students { get; set; }
+    }
+}
---
+++ S24Week12CodeFirstApproach/StudentContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S24Week12CodeFirstApproach
+{
+    // context class
+    public class StudentContext : DbContext
+    {
+        // entity sets
+        // DbSet properties
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Standard> Standards { get; set; }
+
+
+        // define connection string
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB; Database=SchoolEfCore; Trusted_Connection=True;MultipleActiveResultSets=true;");
+        }
+
+
+        // data seed
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Standard>().HasData(
+                new Standard { StandardId = 1, Name = "Standard 1" },
+                new Standard { StandardId = 2, Name = "Standard 2" },
+                new Standard { StandardId = 3, Name = "Standard 3" }
+            );
+
+            modelBuilder.Entity<Student>().HasData(
+                new Student { StudentId = 1, Name = "John", StandardId = 1 },
+                new Student { StudentId = 2, Name = "Anne", StandardId = 1 },
+                new Student { StudentId = 3, Name = "Mark", StandardId = 2 }
+            );
+        }
+    }
+}
